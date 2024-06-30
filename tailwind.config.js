eff424850fe893c38771429cf8f174efb3480c6d
reposlayer <163928307+reposlayer@users.noreply.github.com>
2024-07-01 01:52:49 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#E54033',
+          secondary: '#F2C94C',
+          background: '#1B1A17',
+          surface: '#2C2B28',
+          text: {
+            primary: '#FFFFFF',
+            secondary: '#B2B2B2',
+          },
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
